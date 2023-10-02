c912c2427c70203608afe90995e98be4abecd1e3
AxelothLeohryn <alejandro97ss@gmail.com>
2023-10-02 17:36:15 +0200

    Completado ejercicio sin bonus

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
-console.log(document.title);
+document.querySelector('.infocard');
+let tituloGen1 = document.querySelector('#gen-1');
+tituloGen1.innerHTML = "Generasión 1 Pokimon";
+
+let primerPokemon = document.querySelector('.infocard');
+primerPokemon.style.backgroundColor = 'red';
+
+let currentURL = document.location.href;
+console.log(currentURL);
+
+let currentDomain = document.location.hostname;
+console.log(currentDomain);
+
+console.log(document.querySelectorAll('img'));
+
+let images = document.querySelectorAll('img');
+for (let i = 0; i < images.length; i++) {
+    images[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+}
+
+let infocard = document.querySelectorAll('.infocard-lg-data text-muted');
+for (let i = 0; i < infocard.length; i++) {
+    if (infocard[i].querySelector('itype flying')) {
+        infocard[i].computedStyleMap.backgroundColor = 'red';
+    }
+}
