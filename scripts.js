514a47cec652346bc25ec45c0ba9792dc83665ce
AxelothLeohryn <alejandro97ss@gmail.com>
2023-10-04 11:13:13 +0200

    Solucionado ejercicio bonus

--- scripts.js
+++ scripts.js
@@ -2,8 +2,10 @@ document.querySelector('.infocard');
 let tituloGen1 = document.querySelector('#gen-1');
 tituloGen1.innerHTML = "Generasión 1 Pokimon";
 
-let primerPokemon = document.querySelector('.infocard');
-primerPokemon.style.backgroundColor = 'red';
+let primerPokemon = document.querySelectorAll('.infocard');
+for (let i = 0; i < primerPokemon.length; i++) {
+    primerPokemon[i].style.backgroundColor = 'red';
+}
 
 let currentURL = document.location.href;
 console.log(currentURL);
@@ -18,9 +20,8 @@ for (let i = 0; i < images.length; i++) {
     images[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
 }
 
-let infocard = document.querySelectorAll('.infocard-lg-data text-muted');
+let infocard = document.querySelectorAll('.flying');
 for (let i = 0; i < infocard.length; i++) {
-    if (infocard[i].querySelector('itype flying')) {
-        infocard[i].computedStyleMap.backgroundColor = 'red';
-    }
+        infocard[i].parentElement.parentElement.parentElement.style.backgroundColor = 'yellow';
 }
+
